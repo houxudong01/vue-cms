04d80187b2d37896c872f9bcc419233c21a67332
houxudong <1056752396@qq.com>
2020-05-08 22:09:42 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="app-container">
+        <!-- 顶部 Header 区域 -->
+        <mt-header fixed title="黑马程序员·Vue项目">
+        </mt-header>
+
+        <!-- 底部 Tabbar 区域 -->
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item" href="#">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item" href="#">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">会员</span>
+            </a>
+            <a class="mui-tab-item" href="#">
+                <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+                    <span class="mui-badge" id="badge"></span>
+                </span>
+                <span class="mui-tab-label">购物车</span>
+            </a>
+            <a class="mui-tab-item" href="/search">
+                <span class="mui-icon mui-icon-search"></span>
+                <span class="mui-tab-label">搜索</span>
+            </a>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    .app-container {
+        padding-top: 200px;
+    }
+</style>
